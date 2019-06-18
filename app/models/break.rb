bd5505bd38cfe86b1392aa46ca5cfb71052c3d6a
Milan <milan.dhar@gmail.com>
2019-06-17 17:15:40 -0700

    added activate and deactivate break models

--- app/models/break.rb
+++ app/models/break.rb
@@ -1,3 +1,21 @@
 class Break < ApplicationRecord
   belongs_to :user
+
+
+  def count_break_time
+
+  end
+
+  def deactivate
+    if self.active == true
+      self.update(active: false)
+    end
+  end
+
+  def activate
+    if self.active == false
+      self.update(active: true)
+    end
+  end
+
 end
