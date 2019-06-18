523f3c0759c84c70300466da34bdce607ea7f1b4
Milan <milan.dhar@gmail.com>
2019-06-18 09:07:30 -0700

    added total seconds method

--- app/models/break.rb
+++ app/models/break.rb
@@ -1,9 +1,12 @@
 class Break < ApplicationRecord
   belongs_to :user
 
+  def count_total_break_seconds
+    self.updated_at - self.created_at
+  end
 
-  def count_break_time
-    total_seconds = self.updated_at - self.created_at
+  def count_formatted_break_time
+    total_seconds = self.count_total_break_seconds
     seconds = (total_seconds) % 60
     minutes = (total_seconds / 60) % 60
     hours = total_seconds / (60*60)
