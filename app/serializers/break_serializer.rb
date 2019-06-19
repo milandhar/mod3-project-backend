702d4629febb9e758607f191a148a16aafbb3ddc
Milan <milan.dhar@gmail.com>
2019-06-19 14:28:24 -0700

    changed break serializer to belongs to

--- app/serializers/break_serializer.rb
+++ app/serializers/break_serializer.rb
@@ -1,3 +1,4 @@
 class BreakSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :active, :chosen_url, :chosen_break_time, :created_at, :updated_at 
+  belongs_to :user
+  attributes :id, :user_id, :active, :chosen_url, :chosen_break_time, :created_at, :updated_at
 end
