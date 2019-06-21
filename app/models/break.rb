23f10247572e1b9f244a9950e55d4baa6fcb80b5
Milan <milan.dhar@gmail.com>
2019-06-21 11:07:58 -0700

    changed text

--- app/models/break.rb
+++ app/models/break.rb
@@ -22,7 +22,7 @@ class Break < ApplicationRecord
     @client.api.account.messages.create(
       from: '+12406410870',
       to: '+1' + self.phone_number,
-      body: "Hey #{self.user.user_name}! Your break is up, we've opened: #{self.chosen_url}"
+      body: "Hey #{self.user.user_name}, your break is up! We've opened: #{self.chosen_url}"
     )
   end
 
